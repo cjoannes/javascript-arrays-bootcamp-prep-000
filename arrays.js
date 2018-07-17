636f937c9ffb2c9dc0eba72f3a8dde33d74fceee
cjoannes@tulane.edu <cjoannes@tulane.edu>
2018-07-17 17:37:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element) { return ["foo", ...array] }
 function destructivelyAddElementToBeginningOfArray(array, element) { return ["foo", ...array] }
 function addElementToEndOfArray(array, element) { return [...array, "foo"] }
-function destructivelyAddElementToEndOfArray(array, element) { array.unshift('foo'); return array }
+function destructivelyAddElementToEndOfArray(array, element) { array.unshift(); return array }
 function accessElementInArray(array, index) { return array,index }
 function destructivelyRemoveElementFromBeginningOfArray(array) { array.shift(); return array }
 function removeElementFromBeginningOfArray(array) { return array.slice(-2) }
