72609481edc03eff95464536002d2f4aba2602ef
cjoannes@tulane.edu <cjoannes@tulane.edu>
2018-07-17 17:33:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,4 +7,4 @@ function accessElementInArray(array, index) { return array,index }
 function destructivelyRemoveElementFromBeginningOfArray(array) { array.shift(); return array }
 function removeElementFromBeginningOfArray(array) { return array.slice(-2) }
 function destructivelyRemoveElementFromEndOfArray(array) { array.pop(); return array }
-function removeElementFromEndOfArray(array) { array.slice(0, array.length - 1) }
+function removeElementFromEndOfArray(array) { return array.slice(0, array.length - 1) }
