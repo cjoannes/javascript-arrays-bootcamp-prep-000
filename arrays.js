7354e5d7fc0964aab6ee05fb72fb761243db4249
cjoannes@tulane.edu <cjoannes@tulane.edu>
2018-07-17 17:41:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@ function addElementToBeginningOfArray(array, element) { return ["foo", ...array]
 function destructivelyAddElementToBeginningOfArray(array, element) { return ["foo", ...array] }
 function addElementToEndOfArray(array, element) { return [...array, "foo"] }
 function destructivelyAddElementToEndOfArray(array, element) { array.unshift(); return array }
-function accessElementInArray(array, index) { return console.log(array['index']) }
+function accessElementInArray(array, index) { return console.log(array[n]) }
 function destructivelyRemoveElementFromBeginningOfArray(array) { array.shift(); return array }
 function removeElementFromBeginningOfArray(array) { return array.slice(-2) }
 function destructivelyRemoveElementFromEndOfArray(array) { array.pop(); return array }
